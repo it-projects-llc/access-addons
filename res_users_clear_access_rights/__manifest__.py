29c2d13ebe52802ab501ae5030c6745c55ad701c
Eugene Molotov <molotov@it-projects.info>
2022-02-07 15:42:39 +0500

    [ADD] res_users_clear_access_rights
    Co-authored-by: Stanislav Krotov <ufaks@users.noreply.github.com>
    Co-authored-by: Ivan Yelizariev <yelizariev@users.noreply.github.com>
    Co-authored-by: Ilya Ilchenko <mentalko@users.noreply.github.com>
    Co-authored-by: Artem Rafailov <Ommo73@users.noreply.github.com>
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>

---
+++ res_users_clear_access_rights/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Clear User Access rights",
+    "summary": """Useful tool to reset user rights""",
+    "version": "14.0.1.1.0",
+    "author": "IT-Projects LLC",
+    "license": "LGPL-3",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/access-addons",
+    "category": "Access",
+    "images": ["images/clear_user_access_rights.jpg"],
+    "depends": ["base"],
+    "data": ["views.xml"],
+}
