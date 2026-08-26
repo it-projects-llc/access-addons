702e4a26dccff6121efefa495e24cd817bc21b25
Eugene Molotov <molotov@it-projects.info>
2026-08-26 10:25:17 +0500

    [MIG] read_only_user: бэкпорт модуля на 15.0

--- read_only_user/__manifest__.py
+++ read_only_user/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": """Read Only User""",
-    "version": "17.0.0.1.0",
+    "version": "15.0.0.1.0",
     "author": "IT-Projects LLC",
     "support": "apps@it-projects.info",
     "website": "https://github.com/it-projects-llc/access-addons",
